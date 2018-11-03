c255bf6f5eb30487e4dd47154ef9eedb674306d0
cornelia <martacorneliap@gmail.com>
2018-11-03 15:10:14 +0200

    Added DB connection wrapper

--- src/main/DBConnect.java
+++ src/main/DBConnect.java
@@ -1,21 +1,48 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DBConnect {
 
-	private Connection con;
-	private Statement stmt;
-	private ResultSet rs;
-	
 	public DBConnect() {
+		connection = null;
+		statement = null;
+		resultSet = null;
+		username = null;
+		password = null;
+		connectionPath = null;
+	}
+
+	public DBConnect(String username, String password, String connectionPath){
+
+		this.username = username;
+		this.password = password;
+		this.connectionPath = connectionPath;
+
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		}catch(Exception e) {
-			System.out.println("Database error:" + e);
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + connectionPath,
+														  username,
+					                                      password);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
+	public ResultSet query(String query){
+
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultSet;
+	}
+
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
+	private String username;
+	private String password;
+	private String connectionPath;
 }
