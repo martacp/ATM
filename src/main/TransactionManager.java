5791b4cc063edb55667656466c55a30e4469f06d
cornelia <martacorneliap@gmail.com>
2018-11-06 18:43:20 +0200

    TransactionManager class refactor.

--- src/main/TransactionManager.java
+++ src/main/TransactionManager.java
@@ -6,49 +6,38 @@ import java.util.ArrayList;
 
 public class TransactionManager {
 
-    private String FROM_IBAN;
-    private String TO_IBAN;
-    private String currency;
-    private double amount;
-    private int userID;
-    private ArrayList<Transaction> transactionHistory = new ArrayList<Transaction>(10);
+    private DBConnect Connection;
+    private int UserID;
+    private ArrayList<Transaction> Transactions;
 
-
-    public TransactionManager(int id){
-        userID = id;
+    public TransactionManager(int userID){
+        UserID = userID;
+        Transactions = new ArrayList<Transaction>();
+        Connection = new DBConnect("root", "", "atm");
     }
 
-    public void create(String from, String to, String currency, double amount, int userid){
-
-        DBConnect con = new DBConnect("root","", "atm");
+    public void Create(String from, String to, String currency, double amount, int userid){
         String sql = "insert into Transaction(FROM_IBAN, TO_IBAN, Currency, Amount, UserID)" + " values(?,?,?,?,?)";
-        con.insertTransaction(sql, from, to, currency, amount, userid);
-
+        Connection.insertTransaction(sql, from, to, currency, amount, userid);
     }
 
-    public void showHistory(){
+    public ArrayList<Transaction> GetTransanctions(){
+        return Transactions;
+    }
 
-        DBConnect con = new DBConnect("root", "", "atm");
-        String query = "SELECT * FROM Transaction WHERE UserID = '" +  userID + "'";
-        ResultSet result = con.query(query);
+    public void SetTransactions(){
+        String query = "SELECT * FROM Transaction WHERE UserID = '" +  UserID + "'";
+        ResultSet result = Connection.query(query);
         try {
             while(result.next()){
-
-                String from = result.getString("FROM_IBAN");
-                String to = result.getString("TO_IBAN");
-                String curr= result.getString("Currency");
-                double am = result.getDouble("Amount");
-
-                transactionHistory.add(new Transaction(from, to, am, curr));
-
+               Transactions.add(new Transaction(result.getString("FROM_IBAN"),
+                                                 result.getString("TO_IBAN"),
+                                                 result.getDouble("Amount"),
+                                                 result.getString("Currency")
+                                                ));
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        for(int i = 0 ; i < transactionHistory.size(); i++) {
-            //System.out.println("inside the for loop." + i);
-            transactionHistory.get(i).show();
         }
     }
 
